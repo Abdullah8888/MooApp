0e3e49fb6749b4533c2e230014bc325b7f745daa
= <abdullahijimoh3.ja@gmail.com>
2024-02-13 17:39:45 +0100

    first commit

---
+++ MooApp/MooFRW/MooFRW/MooEntry.swift
@@ -0,0 +1,18 @@
+//
+//  MooEntry.swift
+//  MooFRW
+//
+//  Created by Abdullah on 13/02/2024.
+//
+
+import Foundation
+public class MooEntry {
+    
+    public init() {
+        
+    }
+    
+    public func doSomething() {
+        print("Testing from MoFRW")
+    }
+}
---
+++ MooApp/MooFRW/MooFRW/MooFRW.h
@@ -0,0 +1,18 @@
+//
+//  MooFRW.h
+//  MooFRW
+//
+//  Created by Abdullah on 13/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MooFRW.
+FOUNDATION_EXPORT double MooFRWVersionNumber;
+
+//! Project version string for MooFRW.
+FOUNDATION_EXPORT const unsigned char MooFRWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MooFRW/PublicHeader.h>
+
+
